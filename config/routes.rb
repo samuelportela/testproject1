eec984f033cfabb8b2ac703a052e4a5f613c7a0a
Samuel Portela <negociosnainternet@gmail.com>
2013-08-17 02:40:58 -0300

    created Problem crud with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Testproject1::Application.routes.draw do
+  resources :problems
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
